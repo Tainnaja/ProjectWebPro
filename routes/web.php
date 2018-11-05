919dc57f85e283e384dd50cc6ba046d26b6366cc
Unknown <poicaptiondx@gmail.com>
2018-11-05 14:19:28 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/ProductDetail', function () {
+    return view('ProductDetail');
+});
+
+Route::get('/shophome', function () {
+    return view('shophome');
+});
+
